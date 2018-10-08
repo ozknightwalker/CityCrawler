4cd9fd034e74e79a478343ea086e3db8945b0c7f
Jc Din <jcdin@channelfix.com>
2018-10-08 15:23:49 +0800

    add initial crawler class and requirements.txt
    Initial crawler will get all country codes from geonames.org

--- crawl.py
+++ crawl.py
@@ -1,4 +1,8 @@
 from __future__ import unicode_literals
 
+from lib.geonames import Geoname
 
-# TODO add script to crawl geonames
+
+if __name__ == "__main__":
+    geoname = Geoname()
+    geoname.get_all_country_codes()
