5678be254519da520cc57618967b49992e3eeaf1
Jc Din <jcdin@channelfix.com>
2018-10-09 16:59:19 +0800

    update crawler script to crawl generated urls and record all resulting data to a csv file

--- crawl.py
+++ crawl.py
@@ -1,8 +1,25 @@
 from __future__ import unicode_literals
 
 from lib.geonames import Geoname
+from lib.writer import read_csv, dict_to_csv
+
+import multiprocessing
+
+items_to_crawl = set()
+
+
+def crawl_loop(url):
+    geoname = Geoname(url=url['url'])
+    results = geoname.crawl()
+    if results is None:
+        return
+    for item in results:
+        dict_to_csv('results/cities_without_q', item)
 
 
 if __name__ == "__main__":
-    geoname = Geoname()
-    geoname.get_all_country_codes()
+    fields = ('url',)
+    urls = read_csv('results/url_to_crawl', fieldnames=fields)
+
+    pool = multiprocessing.Pool(8)
+    pool.map(crawl_loop, urls)
